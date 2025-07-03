76fbe652fa6f310b54b16c6903c9325687bfcb51
P4kin <sem.svechka2004@gmail.com>
2025-07-03 13:30:58 +0700

    init shadcn/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  imports: { autoImport: false },
+  modules: ["shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
